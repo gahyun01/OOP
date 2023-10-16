9c9e9dab359c3c2844828988c12a142079360362
Nayeonleeeeee <141228244+Nayeonleeeeee@users.noreply.github.com>
2023-10-16 16:00:26 +0900

    Delete ch4 - 7주차 directory

--- ch4 - 7주차/1) 포인터/Circle.h
+++
@@ -1,8 +0,0 @@
-// Çì´õ
-class Circle {
-	int radius;
-public:
-	Circle();
-	Circle(int r);
-	double getArea();
-};
--- ch4 - 7주차/2) 객체 배열/4-2.h
+++
@@ -1,9 +0,0 @@
-class Circle {
-	int radius;
-
-public:
-	Circle();
-	Circle(int r);
-	void setRadius(int r);
-	double getArea();
-};
